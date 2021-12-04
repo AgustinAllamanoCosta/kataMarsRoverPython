cb57a8020c80ef85cf4b62ea35e226d7b8a898de
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2021-12-04 19:32:14 -0300

    Cominit inicial del repo

---
+++ kataTest/test.py
@@ -0,0 +1,29 @@
+'''
+Created on 4 dic. 2021
+
+@author: Gaspar
+'''
+import unittest
+
+class TestMarsRover(unittest.TestCase):
+    
+    def dadoQueReciboUnaPosicionVerificoQueSeInicializaElObjetoConLaMisma(self):
+        
+        rover = MarsRover();
+        
+        rover.setPosicion((1,1),"N")
+        
+        assert rover.getPosicion() == (1,1)
+        assert rover.getFacing() == "N"
+        
+        
+class MarsRover():
+    
+    def setPosicion(self,poss,facing):
+        pass
+    
+    def getPosicion(self):
+        return (None,None)
+    
+    def getFacing(self):
+        return ""
